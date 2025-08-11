6bbb0d2f5cad49ec44ca7870b7b62d3c33f047c8
ehy0419 <2hy0419@naver.com>
2025-08-11 11:47:34 +0900

    fix.findAllMovieId -> findAllByMovie

--- src/main/java/com/jpa_repeat/repository/ReviewRepository.java
+++ src/main/java/com/jpa_repeat/repository/ReviewRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findAllByMovieId(Movie movie);
+    List<Review> findAllMovieId(Movie movie);
 }
